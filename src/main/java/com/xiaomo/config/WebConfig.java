e80f35b32ecb512130c50ed92458a9f3fa24b359
xiaomo <xiaomo_swordsman@163.com>
2025-02-06 14:34:14 +0800

    2025-02-06 第4次提交

--- src/main/java/com/xiaomo/config/WebConfig.java
+++ src/main/java/com/xiaomo/config/WebConfig.java
@@ -15,6 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 添加自定义的拦截器，并排除一些不需要拦截的url
-        registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login","/user/register");
+        registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login","/user/register","/user/testUserController");
     }
 }
